4279bbe10926fd8829046857b7f634a08e362d1f
Cristian Leon <cristian.leon.avila@gmail.com>
2023-06-21 19:06:20 -0500

    Heroes App

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {path: 'login', component: LoginPageComponent},
+      {path: 'sign-up', component: SignUpComponent},
+      {path: '**', redirectTo: 'login'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+import { SearchHeroPageComponent } from './pages/search-hero-page/search-hero-page.component';
+import { HeroListPageComponent } from './pages/hero-list-page/hero-list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {path: 'new-hero',component: NewHeroPageComponent},
+      {path: 'search',component: SearchHeroPageComponent},
+      {path: 'edit/:id', component: NewHeroPageComponent},
+      {path: 'list', component: HeroListPageComponent},
+      {path: ':id', component: HeroPageComponent},
+      {path: '**', redirectTo: 'list'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
